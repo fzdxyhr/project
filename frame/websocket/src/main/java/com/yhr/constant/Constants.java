02808b925a9fb80511de19f32a1f471b7a3c74ef
yanhr <yanhr@101.com>
2016-07-20 18:40:44 +0800

    新增数据库配置

---
+++ frame/websocket/src/main/java/com/yhr/constant/Constants.java
@@ -0,0 +1,12 @@
+package com.yhr.constant;
+
+/**
+ * @author yhr
+ * @version latest
+ * @date 2016/7/20
+ * @description
+ */
+public class Constants {
+
+    public static final String WEBSOCKET_USERNAME = "web_socket_username";
+}
